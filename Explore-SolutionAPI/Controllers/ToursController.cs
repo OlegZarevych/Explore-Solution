835f369cb83ca62c56bc43a91167fa80fa9ad42a
Oleg <oleg.zarevych@gmail.com>
2019-07-26 20:21:19 +0300

    initial Tour Controller implementation

--- Explore-SolutionAPI/Controllers/ToursController.cs
+++ Explore-SolutionAPI/Controllers/ToursController.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Explore.DataAccess.Abstraction.Entities;
+﻿using Explore.Services.Abstraction;
+using ExploreSolution.DTO;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -13,13 +10,25 @@ namespace ExploreSolution.API.Controllers
     [ApiController]
     public class ToursController : ControllerBase
     {
+        private ITourService tourService;
 
+        public ToursController(ITourService tourService)
+        {
+            this.tourService = tourService;
+        }
 
         [Authorize]
         [HttpPost]
-        public void Add([FromBody] TourEntity tour)
+        public ActionResult Add([FromBody] TourDto tour)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
 
+            tourService.AddTour(tour);
+
+            return Ok();
         }
     }
 }
