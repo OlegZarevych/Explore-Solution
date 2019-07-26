ec92af43949033ed46e345936834021d6ec82faf
Oleg <oleg.zarevych@gmail.com>
2019-07-26 20:20:59 +0300

    move DTOs

---
+++ Explore.Dto.Abstraction/DTO/TourDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExploreSolution.DTO
+{
+    public class TourDto
+    {
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        // [Column(TypeName = "money")]
+        public decimal Price { get; set; }
+
+        // [Column(TypeName = "varchar(MAX)")]
+        public string Notes { get; set; }
+    }
+}
