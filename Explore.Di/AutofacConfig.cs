fe918c340d36c925c7b3397155ea193dad903ee2
Oleg <oleg.zarevych@gmail.com>
2019-07-27 21:05:33 +0300

    add GraphAPI and DI proj

---
+++ Explore.Di/AutofacConfig.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Explore.Di
+{
+    public class AutofacConfig
+    {
+        public static void ConfigureContainer()
+        {
+            var builder = new ContainerBuilder();
+
+
+            var containerr = builder.Build();
+
+        }
+    }
+}
