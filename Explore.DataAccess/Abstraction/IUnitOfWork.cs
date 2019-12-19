7b4bb2272bda0f304faa1d6829dbc939aa934702
Oleg <oleg.zarevych@gmail.com>
2019-12-20 01:38:57 +0200

    add potential UnitOfWork

---
+++ Explore.DataAccess/Abstraction/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Explore.DataAccess.Abstraction
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        Task<int> CommitAsync();
+
+        ITourRepository TourRepository { get; set; }
+    }
+}
---
+++ Explore.DataAccess/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using Explore.DataAccess.Abstraction;
+using System.Threading.Tasks;
+
+namespace Explore.DataAccess
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ExploreDb db;
+
+        public UnitOfWork(ExploreDb dbContext, ITourRepository tourRepo)
+        {
+            this.db = dbContext;
+            this.TourRepository = tourRepo;
+        }
+
+        public ITourRepository TourRepository { get; set; }
+
+        public async Task<int> CommitAsync()
+        {
+            return await db.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+    }
+}
