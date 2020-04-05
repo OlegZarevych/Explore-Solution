4ff6db056f66f933bbffccc5a83f313bc95e4a72
Oleg <oleg.zarevych@gmail.com>
2020-04-05 13:23:28 +0300

    add MSpec project

---
+++ Tests/Explore.Specs/When_Add_New_Reservation.cs
@@ -0,0 +1,27 @@
+﻿using Explore.Services.Services;
+using Machine.Specifications;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Explore.Specs
+{
+    [Subject("Reservation")]
+    public class When_Add_New_Reservation
+    {
+        Establish context = () =>
+        {
+
+        };
+
+        Because of = () =>
+        {
+
+        };
+
+        It Should_Add_Valid_Tour = () =>
+        {
+
+        };
+    }
+}
