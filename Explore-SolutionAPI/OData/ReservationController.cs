581a37a70fd5869a67584d1399b6742a59415ba3
Oleg <oleg.zarevych@gmail.com>
2020-04-07 20:15:25 +0300

    add another OData endpoints, but it's doesn't work :(

--- Explore-SolutionAPI/OData/ReservationController.cs
+++ Explore-SolutionAPI/OData/ReservationController.cs
@@ -20,11 +20,33 @@ namespace ExploreSolution.API.OData
         [Authorize]
         [HttpGet]
         [EnableQuery]
-        public async Task<IQueryable<Reservation>> GetReservations()
+        public async Task<IQueryable<Reservation>> Get()
         {
             var result = await reservationService.GetAllReservationsAsync();
 
             return result.AsQueryable();
         }
+
+        //[Authorize]
+        //[HttpDelete]
+        //[EnableQuery]
+        //public async Task<IQueryable<Reservation>> Delete([FromODataUri]string key)
+        //{
+        //    var result = await reservationService.GetAllReservationsAsync();
+
+        //    return result.Where(r => r.Id == Convert.ToInt32(key)).AsQueryable();
+        //}
+
+        //[Authorize]
+        //[HttpPatch]
+        //[EnableQuery]
+        //public async Task<Reservation> PatchReservations(int key, Delta<Reservation> patch)
+        //{
+        //    var result = await reservationService.GetAllReservationsAsync();
+        //    var reservation = result.AsQueryable().FirstOrDefault(r => r.Id == key);
+        //    patch.Patch(reservation);
+
+        //    return reservation;
+        //}
     }
 }
